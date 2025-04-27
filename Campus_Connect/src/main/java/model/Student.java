02ba3e9b07d0253f6cd54dd82fe09d608ea9c900
somjena <jenasoumitrapvt@gmail.com>
2025-04-28 01:08:09 +0530

    Update Student.java

---
+++ Campus_Connect/src/main/java/model/Student.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Student extends User {
+    private String branch;
+    private int semester;
+    private double cgpa;
+    private String skills;
+    private String resumePath;
+
+    // Constructors, getters, and setters
+    public Student() {}
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public String getResumePath() {
+        return resumePath;
+    }
+
+    public void setResumePath(String resumePath) {
+        this.resumePath = resumePath;
+    }
+}
